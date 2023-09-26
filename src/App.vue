593445719472686640eb422e506bfe376b8ba4f6
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-26 14:06:03 +0400

    start project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<NotesComponent />
+</template>
+
+<script>
+import NotesComponent from './components/NotesComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NotesComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NotesComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "NotesComponent",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
