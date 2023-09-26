8bd76bbb240355cae6d6812b048f360b514a644a
PaulRomanov <pavel.ramanau@gmail.com>
2023-09-26 15:01:15 +0400

    add ready project

--- src/components/NotesComponent.vue
+++ src/components/NotesComponent.vue
@@ -1,17 +1,84 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Список заметок</h1>
+    <input
+      type="text"
+      class="custom-input"
+      v-model="valueModel"
+      @keypress.enter="addNewNote"
+    />
+    <ul class="note-list" v-if="notes.length !== 0">
+      <li class="note-item" v-for="(note, index) in notes" :key="index">
+        {{ note }}
+        <button @click="removeNote(index)" class="remove-button">Удалить</button>
+      </li>
+    </ul>
+    <div v-else>Введите новую заметку</div>
+    <hr />
+    <button @click="addNewNote" class="add-button">Добавить заметку</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "NotesComponent",
-  created() {},
   data() {
-    return {};
+    return {
+      notes: ["заметка 1", "заметка 2", "заметка 3"],
+      valueModel: "",
+    };
   },
-  props: {},
-  methods: {},
+  methods: {
+    addNewNote() {
+      if (this.valueModel !== "") {
+        this.notes.push(this.valueModel);
+        this.valueModel = "";
+      }
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.custom-input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.note-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.note-item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+}
+
+.add-button {
+  background-color: green;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-right: 10px;
+}
+
+.remove-button {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  font-size: 12px;
+  cursor: pointer;
+  margin-left: 10px;
+}
+</style>
